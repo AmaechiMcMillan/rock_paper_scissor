8baba9056a5a225557750975dd9aa6d9c12a88b3
Amaechi <amaechimcmillan@gmail.com>
2021-07-17 23:30:13 -0700

    intial commit

---
+++ rpsls.py
@@ -0,0 +1,25 @@
+# (dev)make at least 10 commits
+# (dev)properly incorporate inheritance
+# (dev)account for and handle bad user input, ensure that user input is validated and reobtained if necessary
+# (dev)store gesture options in a list. Find a way to utilize list of gestures with my code
+# (player)correct player will win a given round based on the choices made by each player
+# (player)'best of three' to decide a winner
+# (player)option of single player(human vs AI) or multiplayer(human vs human) game. 
+
+# Display a welcome message
+# Select single player (human vs AI) or multiplayer (human vs human)
+# Display a list of gesture options
+# Players select gestures
+# Compare the gestures and see which one wins (also check for ties)
+# Show the winner of the round and track the score of the winner
+# See if anyone has won the entire game
+# IF NOT, play another round
+# When someone has won best of three
+# Display the winner
+
+# CLASSES
+# Game(like battlefield)
+# Player(parent)
+# AI(child of player)
+# Human (child of player)
+
