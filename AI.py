1c2f5b0a2de149d8e980a2c0bcae8a9c7d65dbaf
TaborMather-dev <86973131+TaborMather-dev@users.noreply.github.com>
2021-07-20 13:59:43 -0700

    Update AI.py
    Added 'gestures' class property, and 'choose_gesture' method.

--- AI.py
+++ AI.py
@@ -3,7 +3,16 @@ from player import Player
 class AI(Player):
     def __init__(self):
         self.name = ("CPU")
+        self.gestures = ["rock", "paper", "scissors", "lizard", "spock"]
+        self.chosen_gesture = ""
+        self.choose_gesture = ()
         self.score = 0
 
+    def choose_gesture(self):
+        gestures = ["rock", "paper", "scissors", "lizard", "spock"]
+        self.chosen_gesture = map(
+            gestures, ("rock", "paper", "scissors", "lizard", "spock"))
+        return len(self.gestures)
+        
     def increment_score(self):
         return super().increment_score()
