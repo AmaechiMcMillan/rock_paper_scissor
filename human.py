fd8748e9c7aa49ee09c7f8daa0795caec68f69cc
TaborMather-dev <86973131+TaborMather-dev@users.noreply.github.com>
2021-07-20 13:54:01 -0700

    Update human.py
    Added 'gestures', defined 'choose_gesture' method.

--- human.py
+++ human.py
@@ -3,10 +3,19 @@ from player import Player
 class Human(Player):
     def __init__(self):
         self.name = ("Jimbo")
+        self.gestures = ["rock", "paper", "scissors", "lizard", "spock"]
+        self.chosen_gesture = ""
+        self.choose_gesture = ()
         self.score = 0
 
     def show_name(self):
         return self.name 
+    
+    def choose_gesture(self):
+        gestures = ["rock", "paper", "scissors", "lizard", "spock"]
+        self.chosen_gesture = map(
+            gestures, ("rock", "paper", "scissors", "lizard", "spock"))
+        return len(self.gestures)
     
     def increment_score(self):
         return super().increment_score()
